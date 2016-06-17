c2a9f649a2f27eda20b165349229bfde13fe1682
Kevin Tucker <kwtucker4@gmail.com>
2016-06-16 22:18:44 -0400

    Added newline

--- test/__util.js
+++ test/__util.js
@@ -27,6 +27,7 @@ describe('Util Debug Tool', () => {
     done();
   })
 
+
   // Test for the debug tool to make sure it
   // passes the title, object and status to the logfile.log
   it('Should read the logfile.log file to check if the title, object, and status are there', (done) => {
