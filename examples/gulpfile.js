8c0366828a69d604ddeb133a4b52a42c3518c784
ptlergo <ptlergo@gmail.com>
2016-06-24 15:45:46 -0400

    moved gulp file

--- examples/gulpfile.js
+++ examples/gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
-const kutil = require('./src/kutil');
-const package = require('./package.json');
+const kutil = require('../src/kutil');
+const package = require('../package.json');
 
 gulp.task('patch', () => {
   let ver = package.version;
